f0cbb9424aa07202e87b39bd2f6df9834ca6a9d0
MaxonZhao <maxonzhao@gmail.com>
2022-05-17 16:01:11 -0700

    chore: setting up the project

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// route handler of express
+app.get('/', (req, res) => {
+    res.send({hi: 'there'})
+});
+
+// runtime environment parameters, Heroku will dynamically allocate a port number
+// by setting the runtime PORT number 
+// Or set it to port 5000 instead if we are in development setting - not deploying on Heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
